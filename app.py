e283474c81d2e9dc29ed498fa490cc08e4322c37
Md Afique Amin Zian <44741072+zianafique@users.noreply.github.com>
2022-12-10 19:10:54 +0600

    add web app file

---
+++ app.py
@@ -0,0 +1,34 @@
+import json
+import pickle
+
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+import logging
+
+logging.basicConfig(filename='app_log.log', filemode='a',
+                    format='%(asctime)s -%(levelname)s- %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S', level=logging.DEBUG)
+
+app=Flask(__name__)
+## Load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+   
